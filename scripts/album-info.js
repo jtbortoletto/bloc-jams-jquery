7a880671eda65586d4ff1ac9d2c2e98b453a5468
John Bortoletto <jtbortoletto@yahoo.com>
2018-02-21 18:29:20 -0600

    album info edit

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('#album-data').text(album.artist);
+  $('#album-data').text(album.releaseInfo);
 }
